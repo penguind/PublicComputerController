2f3c90af9f5ccd7f95650a9606d12ebf49e5e09f
Penguind <penguind@users.noreply.github.com>
2016-02-28 01:14:18 +0800

    Add regist PC form

---
+++ client/IfMyComputer/IfMyComputer/Configer.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace IfMyComputer
+{
+    class Configer
+    {
+        public string serverIP;
+        public int serverPort;
+        public string prefixOfMonitoredIP;
+        public string secretKey;
+
+        private string errorMsg;
+
+        public Configer()
+        {
+            serverIP = "127.0.0.1";
+            serverPort = 9527;
+            prefixOfMonitoredIP = "127.";
+            secretKey = "";
+
+            errorMsg = "";
+        }
+
+        /// <summary>
+        /// Read configuration from "config.ini", which user defined.
+        /// </summary>
+        /// <returns>If error occurred, will return false and errorMsg will be recorded.Normal true.</returns>
+        public bool loadConfig()
+        {
+            try
+            {
+                using (FileStream ifs = new FileStream("config.ini", FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(ifs))
+                    {
+                        string line = "";
+                        string temp = "";
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            if (line.StartsWith("serverip="))
+                            {
+                                temp = line.Split('=')[1];
+                                serverIP = NetworkUtil.validateIP(temp) ? temp : "127.0.0.1"; //bad ip is regarded as normal ip;
+                            }
+                            if (line.StartsWith("serverport="))
+                            {
+                                if (int.TryParse(line.Split('=')[1], out serverPort) == false)
+                                {
+                                    serverPort = 9527; //bad port is regarded as normal port
+                                }
+                            }
+                        }
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                errorMsg = e.Message;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Write "config.ini", user difined configuration will be saved.
+        /// </summary>
+        /// <returns>If error occurred, will return false and errorMsg will be recorded.Normal true.</returns>
+        public bool writeConfig()
+        {
+            try
+            {
+                using (FileStream ofs = new FileStream("config.ini", FileMode.Create))
+                {
+                    using (StreamWriter sw = new StreamWriter(ofs))
+                    {
+                        sw.WriteLine("serverip=" + serverIP);
+                        sw.WriteLine("serverport" + serverPort);
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                errorMsg = e.Message;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Read configuration from "preconfig.ini", which administrator defined or software auto recorded.
+        /// </summary>
+        /// <returns>If error occurred, will return false and errorMsg will be recorded.Normal true.</returns>
+        public bool loadPreConfig()
+        {
+            try
+            {
+                using (FileStream ifs = new FileStream("preconfig.ini", FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(ifs))
+                    {
+                        string line = "";
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            if (line.StartsWith("secretkey="))
+                            {
+                                secretKey = line.Split('=')[1];
+                            }
+                            if (line.StartsWith("prefixOfIP="))
+                            {
+                                prefixOfMonitoredIP = line.Split('=')[1];
+                            }
+                        }
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                errorMsg = e.Message;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Write "preconfig.ini", administrator difined configuration will be saved, the software auto recorded will be refreshed to current statues.
+        /// </summary>
+        /// <returns>If error occurred, will return false and errorMsg will be recorded.Normal true.</returns>
+        public bool writePreConfig()
+        {
+            try
+            {
+                using (FileStream ofs = new FileStream("preconfig.ini", FileMode.Create))
+                {
+                    using (StreamWriter sw = new StreamWriter(ofs))
+                    {
+                        sw.WriteLine("prefixOfIP=" + prefixOfMonitoredIP.Trim());
+                        sw.WriteLine("secretkey=" + Auth.getSecretKey());
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                errorMsg = e.Message;
+                return false;
+            }
+        }
+    }
+}
---
+++ client/IfMyComputer/IfMyComputer/RegistForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace IfMyComputer
+{
+    partial class RegistForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.serverIPTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serverPortTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "服务器IP：";
+            // 
+            // serverIPTextBox
+            // 
+            this.serverIPTextBox.Location = new System.Drawing.Point(83, 20);
+            this.serverIPTextBox.Name = "serverIPTextBox";
+            this.serverIPTextBox.Size = new System.Drawing.Size(124, 21);
+            this.serverIPTextBox.TabIndex = 1;
+            this.serverIPTextBox.Text = "192.168.123.145";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(213, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = ":";
+            // 
+            // serverPortTextBox
+            // 
+            this.serverPortTextBox.Location = new System.Drawing.Point(230, 20);
+            this.serverPortTextBox.Name = "serverPortTextBox";
+            this.serverPortTextBox.Size = new System.Drawing.Size(50, 21);
+            this.serverPortTextBox.TabIndex = 3;
+            this.serverPortTextBox.Text = "12345";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "符合规则的IP:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Location = new System.Drawing.Point(101, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "（IP前缀为123.123.123.123）";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.comboBox1.Items.AddRange(new object[] {
+            "123.",
+            "456."});
+            this.comboBox1.Location = new System.Drawing.Point(14, 70);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(266, 20);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "本机别名：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(197, 21);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "本机备注：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(14, 144);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(266, 88);
+            this.textBox2.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "注 册";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // RegistForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.serverPortTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.serverIPTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "RegistForm";
+            this.Text = "注册新PC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox serverIPTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox serverPortTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
